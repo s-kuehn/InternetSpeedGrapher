ca8b0dbbe3f29a65b0d547f71cbfa02e5d843c99
s-kuehn <amazingsdk@gmail.com>
2020-10-25 14:06:39 -0400

    Added sqlite database.

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ import speedtest as st
 import pandas as pd
 import sqlite3
 
+# Create and connect to db file
+con = sqlite3.connect('./results.db')
+c = con.cursor()
+
+# Create table in db if it doesn't already exist
+c.execute("""CREATE TABLE IF NOT EXISTS results (
+    ping INTEGER,
+    download_mbs INTEGER,
+    upload_mbs INTEGER 
+    ) """)
+
+# Commit changes to db
+con.commit()
+
+# Read db into pandas
+df = pd.read_sql_query('SELECT * FROM results', con)
+
+print(df)
+
+# Get internet speed
 def getSpeed():
     speed_test = st.Speedtest()
     speed_test.get_best_server()
@@ -18,4 +38,4 @@ def getSpeed():
 
     return (ping, download_mbs, upload_mbs)
 
-print(getSpeed())
+# print(getSpeed())
